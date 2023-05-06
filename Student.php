8633623e8b4355432cf756da8536162dd0771736
BlackIceNoWay <104631533+XuanHoang03@users.noreply.github.com>
2023-05-06 14:10:17 +0700

    Add files via upload
    Class Student

---
+++ Student.php
@@ -0,0 +1,35 @@
+<?php
+class Student{
+    private int $ID;
+    private int $age;
+    private string $name;
+
+
+    public function setID(int $ID){
+        $this->ID = $ID;
+    }
+    public function setAge(int $age){
+        $this->age = $age;
+    }
+    public function setName(string $name){
+        $this->name = $name;
+    }
+
+    function __construct(int $ID , int $age , string $name){
+        $this->setAge($age);
+        $this->setID($ID);
+        $this->setName($name);
+    }
+    function getId(){
+        return $this->ID;
+    }
+    function getAge(){
+        return $this->age;
+    }
+
+    function getName(){
+        return $this->name;
+    }
+
+}
+?>
