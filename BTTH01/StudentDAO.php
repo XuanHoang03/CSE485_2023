f59731ca2c4257b84e4e6d0b2b32ca994ac1909b
BlackIceNoWay <104631533+XuanHoang03@users.noreply.github.com>
2023-05-10 17:11:25 +0700

    Update1

---
+++ BTTH01/StudentDAO.php
@@ -0,0 +1,51 @@
+<?php
+class StudentDAO{
+
+    public array $_groups;
+
+    function __construct(){
+        $this->_groups = [];
+    }
+
+    public function create(Student $student ){
+         array_push($this->_groups,$student);
+    }
+
+    public function read(Student $student){
+        echo "ID: " . $student->getId() ."<br>Name: " . $student->getName() . "<br>Age: " . $student->getAge() ; 
+    }
+
+    public function getElementById($ID){
+        $array = $this->_groups;
+        foreach( $array as $elements )
+        {
+            if($elements->getId() == $ID){
+                return $elements;
+            }
+        }
+        return false;
+    }
+
+    public function updateAgeByID(int $ID,int $age){
+        $this->getElementById($ID)->setAge($age);
+    }
+    public function updateNameByID(int $ID,string $name){
+        $this->getElementById($ID)->setName($name);
+    }
+    public function deleteByID($ID){
+    $studentTemp = $this->getElementById($ID);
+    $key = array_search($studentTemp, $this->_groups);
+    if($key !== false)
+        {
+        unset($this->_groups[$key]);    
+        } 
+    }
+    public function GetAll(){
+        foreach( $this->_groups as $elements){
+            echo "<br>";
+            $this->read($elements);
+        }
+    }
+}
+?>
+
